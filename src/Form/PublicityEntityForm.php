849969cd843078415dbba04a7b56b4b8afc1266f
Jackson <jackson.parada@globant.com>
2019-01-18 16:11:33 -0500

    hook_form_alter

--- src/Form/PublicityEntityForm.php
+++ src/Form/PublicityEntityForm.php
@@ -3,8 +3,11 @@
 namespace Drupal\publicity\Form;
 
 use Drupal\Core\Entity\EntityForm;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\core\modules\taxonomy\src\Entity\Vocabulary;
+use Drupal\Component\Utility\Number as NumberUtility;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class PublicityEntityForm.
@@ -19,6 +22,7 @@ class PublicityEntityForm extends EntityForm {
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
 
+    $class = get_class($this);
     $publicity_entity = $this->entity;
     $form['label'] = [
       '#type' => 'textfield',
@@ -27,6 +31,9 @@ class PublicityEntityForm extends EntityForm {
       '#default_value' => $publicity_entity->label,
       '#description' => $this->t("Name for the Publicity."),
       '#required' => TRUE,
+      '#element_validate'=>[
+        [$class, 'validateString'],
+      ],
     ];
 
     $form['id'] = [
@@ -44,8 +51,7 @@ class PublicityEntityForm extends EntityForm {
       '#type' => 'textfield',
       '#title' => $this->t('Identificator'),
       '#description' => $this->t("ID the publicity"),
-      '#placeholder' => '8c4...'
-
+      
     ];
 
     $form['UrlPu'] = [
@@ -103,6 +109,9 @@ class PublicityEntityForm extends EntityForm {
         '#required' => TRUE,
         '#default_value' => $width['form'][$delta]['width'],
         '#description' => $this->t('The width in px.'),
+        '#element_validate'=>[
+          [$class, 'validateNumber'],
+        ],
       ];
   
       $form['breakpoints']['form'][$delta]['height'] = [
@@ -112,6 +121,9 @@ class PublicityEntityForm extends EntityForm {
         '#required' => TRUE,
         '#default_value' => $width['form'][$delta]['height'],
         '#description' => $this->t('The height in px.'),
+        '#element_validate'=>[
+          [$class, 'validateNumber'],
+        ],
       ];
   
       $form['breakpoints']['form'][$delta]['remove'] = [
@@ -136,6 +148,9 @@ class PublicityEntityForm extends EntityForm {
     ];
     return $form;
   }
+  /**
+   * 
+   */
   /**
    * function to add one field of breakpoint.
    * 
@@ -198,6 +213,52 @@ class PublicityEntityForm extends EntityForm {
     $form_state->setRebuild();
     
   }
+
+  public static function validateNumber(&$element, FormStateInterface $form_state, &$complete_form) {
+    $value = $element['#value'];
+    if ($value === '') {
+      return;
+    }
+    $name = empty($element['#title']) ? $element['#parents'][0] : $element['#title'];
+    // Ensure the input is numeric.
+    if (!is_numeric($value)) {
+      $form_state->setError($element, t('%name must be a number.', ['%name' => $name]));
+      return;
+    }
+    // Ensure that the input is greater than the #min property, if set.
+    if (isset($element['#min']) && $value < $element['#min']) {
+      $form_state->setError($element, t('%name must be higher than or equal to %min.', ['%name' => $name, '%min' => $element['#min']]));
+    }
+    // Ensure that the input is less than the #max property, if set.
+    if (isset($element['#max']) && $value > $element['#max']) {
+      $form_state->setError($element, t('%name must be lower than or equal to %max.', ['%name' => $name, '%max' => $element['#max']]));
+    }
+    if (isset($element['#step']) && strtolower($element['#step']) != 'any') {
+      // Check that the input is an allowed multiple of #step (offset by #min if
+      // #min is set).
+      $offset = isset($element['#min']) ? $element['#min'] : 0.0;
+      if (!NumberUtility::validStep($value, $element['#step'], $offset)) {
+        $form_state->setError($element, t('%name is not a valid number.', ['%name' => $name]));
+      }
+    }
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function validateString(&$element, FormStateInterface $form_state, &$complete_form) {
+    $value = $element['#value'];
+    $value = strtolower($value);
+    if (!preg_match('/^[a-z ]{3,15}$/', $value)) {
+      $form_state->setError($element, t('Please. Write only data type string. Minimum 3 characters and Maximum 15'));
+    }
+  }
+  public static function validateIdpublicity(&$element, FormStateInterface $form_state, &$complete_form){
+    $value = $element['#value'];
+    $value = strtolower($value);
+    if (!preg_match('/^[a-z0-9]{6}$/', $value)){
+      $form_state->setError($element, t('Please. Write only data type string. Minimum 3 characters and Maximum 6'));
+    }
+  }  
   
   /**
    * @param array $form
@@ -234,6 +295,9 @@ class PublicityEntityForm extends EntityForm {
     $form_state->setRedirectUrl($publicity_entity->toUrl('collection'));
   }
 
+  /**
+   * @return $names = taxonomy Vocabulary added
+   */
   public function taxonomy_vocabulary_get_names() {
     $names =& drupal_static(__FUNCTION__);
     if (!isset($names)) {
@@ -248,6 +312,9 @@ class PublicityEntityForm extends EntityForm {
     return $names;
   }
 
+  /**
+   * @return $names = content types added
+   */
   public function content_type_get_names() {
     $names =& drupal_static(__FUNCTION__);
     if (!isset($names)) {
@@ -264,5 +331,3 @@ class PublicityEntityForm extends EntityForm {
   }
       
 }
-
-// $vocabulary = entity_load('taxonomy_vocabulary', 'MACHINE_NAME')->label();
